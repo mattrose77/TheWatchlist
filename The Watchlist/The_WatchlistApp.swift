2ef35c2d8c1799ca1ebf8e0be70284f87102f9a5
Matt Rose <matt1190rose@hotmail.co.uk>
2026-05-20 21:49:33 +0100

    Initial Commit

---
+++ The Watchlist/The_WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_WatchlistApp.swift
+//  The Watchlist
+//
+//  Created by Matt Rose on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct The_WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
